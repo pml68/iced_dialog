73742d5b374469548e62fca2827616f9e3f118ca
pml68 <pml68@users.noreply.github.com>
2025-03-31 21:32:54 +0000

    deploy: 18462cef6ae1702493ea5d3c6215cb800f9eb2e5

---
+++ trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iced_dialog",[["impl&lt;'a, Message, Theme, Renderer&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"iced_dialog/dialog/struct.Dialog.html\" title=\"struct iced_dialog::dialog::Dialog\">Dialog</a>&lt;'a, Message, Theme, Renderer&gt;<div class=\"where\">where\n    &lt;Theme as Catalog&gt;::Class&lt;'a&gt;: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a>,\n    &lt;Theme as Catalog&gt;::Class&lt;'a&gt;: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a>,\n    &lt;Renderer as Renderer&gt;::Font: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a>,</div>",1,["iced_dialog::dialog::Dialog"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[987]}
---
+++ trait.impl/core/panic/unwind_safe/trait.UnwindSafe.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iced_dialog",[["impl&lt;'a, Message, Theme, Renderer&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/panic/unwind_safe/trait.UnwindSafe.html\" title=\"trait core::panic::unwind_safe::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"iced_dialog/dialog/struct.Dialog.html\" title=\"struct iced_dialog::dialog::Dialog\">Dialog</a>&lt;'a, Message, Theme, Renderer&gt;",1,["iced_dialog::dialog::Dialog"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[434]}
